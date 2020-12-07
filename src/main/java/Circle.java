e83fe8a8d19190934e2b4e8fb3bc4984ef2f60bc
Peniel Chiemerie Okeke <66641750+Chiemerie01@users.noreply.github.com>
2020-12-07 03:00:27 +0100

    Update Circle.java

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -19,7 +19,7 @@ public double getArea() {
     return radius * radius * Math.PI;
 }
 
-public double setRadius(double radius){
+public void setRadius(double radius){
     this.radius = radius;
 }
     
