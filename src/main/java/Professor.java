67d811515e9580e6432064755f3492c0680d8bd7
Peniel Chiemerie Okeke <66641750+Chiemerie01@users.noreply.github.com>
2020-12-07 04:37:26 +0100

    Update Professor.java

--- src/main/java/Professor.java
+++ src/main/java/Professor.java
@@ -5,10 +5,10 @@ public class Professor extends User{
     private int phone = 0;
 private Professor(){}  
 public Professor(String id, String firstname, String lastname) {
-    super(id, Domain.Staff, firstname, lastname);
+    super(id, Domain.STAFF, firstname, lastname);
 } 
 public Professor(String id, String firstname, String lastname, Date birthdate){
-    super(id, Domain.Staff, firstname, lastname, birthdate);
+    super(id, Domain.STAFF, firstname, lastname, birthdate);
 }
 public void setFaculty(Faculty faculty){
     this.faculty = faculty;
@@ -33,10 +33,10 @@ public void setPhone(int extension){
 }
 public String getPhone(){
     if (campus == Campus.KLE){
-        return "+49282180673" +String.valueOf(this.phone);
+        return "+49282180673" + String.valueOf(this.phone);
     }
     else{
-        return "+492842908" +String.valueOf(this.phone);
+        return "+492842908" + String.valueOf(this.phone);
     }
 }
 
